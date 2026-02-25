a445e03270b2af43dc63ad359bbc1a6b8066a3c3
8tox <162425785+8tox@users.noreply.github.com>
2026-02-25 16:56:07 +0530

    Refactor Supabase client initialization and functions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://zbbidcbmqelfdzmyujex.supabase.co";
 const SUPABASE_KEY = "sb_publishable_88HCkBeBTMGM4FQ_BAIr6g_zO3toX8E";
 
-const client = window.supabase.createClient(https://zbbidcbmqelfdzmyujex.supabase.co , sb_publishable_88HCkBeBTMGM4FQ_BAIr6g_zO3toX8E);
+const client = window.supabase.createClient("https://zbbidcbmqelfdzmyujex.supabase.co" , "sb_publishable_88HCkBeBTMGM4FQ_BAIr6g_zO3toX8E");
 
 function toggleDark() {
   document.body.classList.toggle("dark");
@@ -54,6 +54,7 @@ async function submitConfession() {
   input.value = "";
   alert("Confession submitted for review.");
 }
+
 
 
 
